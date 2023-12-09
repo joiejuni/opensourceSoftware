de4f79f6a6b71e1090e6d1062295fe878a532ad2
Leeyoujin00 <cymn6032@gmail.com>
2023-12-09 18:23:26 +0900

    create hello.html

--- src/main/java/com/example/demo/MoolApplication.java
+++ src/main/java/com/example/demo/MoolApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoolApplication {
 
+//	@RequestMapping("/")
+//	String home() {
+//		return "hello";
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(MoolApplication.class, args);
 	}
-
 }
