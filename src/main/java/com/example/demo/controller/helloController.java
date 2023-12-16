1ab695f4d1e61b07f8e7aff1f11232532ffac331
jiwonLee42 <ljw56b@gmail.com>
2023-12-17 05:01:33 +0900

    feat: update user signup ui

--- src/main/java/com/example/demo/controller/helloController.java
+++ src/main/java/com/example/demo/controller/helloController.java
@@ -2,12 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class helloController {
 
     @GetMapping("/signup")
-    public String helloFunction() {
-        return "signup";
+    public ModelAndView helloFunction() {
+        return new ModelAndView("signup");
     }
 }
